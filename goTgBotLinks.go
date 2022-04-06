48d49b5b97b75f06b46e0f191e1d01031ddc887e
OlaMarty <ok9039039898@gmail.com>
2022-04-06 19:43:25 +0700

    пробую вернуть тире

--- goTgBotLinks.go
+++ goTgBotLinks.go
@@ -6,8 +6,8 @@ var goTgBotLinks = `
 <a href="https://ashirobokov.wordpress.com/2016/10/14/dnd-spells-telegram-bot-2/">Как я tgbotа на GO писал</a>
 <a href="https://www.youtube.com/watch?v=AJdHRBG8sT4">Видео Создаем бота</a>
 <a href="https://intensive.skillbox.ru/go/record/">Практикум на skillbox пишем бота</a>
-<a href="https://pkg.go.dev/github.com/go-telegram-bot-api/telegram-bot-api/v5#NewBotAPIWithAPIEndpoint">tgbotAPI основные функции</a>
-<a href="https://core.telegram.org/bots/api#html-style">tgbotAPI про html, стили и шрифты</a>
-<a href="https://pkg.go.dev/github.com/go-telegram-bot-api/telegram-bot-api/v5#MessageConfig">tgbotAPI про Message</a>
+<a href="https://pkg.go.dev/github.com/go-telegram-bot-api/telegram-bot-api/v5#NewBotAPIWithAPIEndpoint">tgbotAPI - основные функции</a>
+<a href="https://core.telegram.org/bots/api#html-style">tgbotAPI - про html, стили и шрифты</a>
+<a href="https://pkg.go.dev/github.com/go-telegram-bot-api/telegram-bot-api/v5#MessageConfig">tgbotAPI - про Message</a>
 <a href="https://github.com/tucnak/telebot">либа с кодом на github</a>
 `
