f6692bc772f4ebde59b975e445a39a94e0405640
OlaMarty <ok9039039898@gmail.com>
2022-04-12 23:21:30 +0700

    исправление ошибки

--- apiLinks.go
+++ apiLinks.go
@@ -21,8 +21,4 @@ var apiLinks = []buttonLink{
 		Name: "Видео-уроки APItesting",
 		Link: "https://www.youtube.com/c/okiseleva/playlists",
 	},
-	{
-		Name: "Список HTTP кодов",
-		Link: "https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D0%BA%D0%BE%D0%B4%D0%BE%D0%B2_%D1%81%D0%BE%D1%81%D1%82%D0%BE%D1%8F%D0%BD%D0%B8%D1%8F_HTTP",
-	},
 }
--- protocolLinks.go
+++ protocolLinks.go
@@ -13,4 +13,8 @@ var protocolLinks = []buttonLink{
 		Name: "Копирование через ssh",
 		Link: "https://wiki.enchtex.info/tools/console/scp",
 	},
+	{
+		Name: "Список HTTP кодов",
+		Link: "https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D0%BA%D0%BE%D0%B4%D0%BE%D0%B2_%D1%81%D0%BE%D1%81%D1%82%D0%BE%D1%8F%D0%BD%D0%B8%D1%8F_HTTP",
+	},
 }
