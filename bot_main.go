6095908df7222ba6df0537cb1e08214c7c22d7dd
OlaMarty <ok9039039898@gmail.com>
2022-05-09 11:50:21 +0700

    t

--- bot_main.go
+++ bot_main.go
@@ -90,7 +90,7 @@ func main() {
 	updates, _ := bot.GetUpdatesChan(u)
 
 	for update := range updates {
-		if update.Message == nil {
+		if update.Message == nil && update.CallbackQuery == nil {
 			continue
 		}
 		/*
