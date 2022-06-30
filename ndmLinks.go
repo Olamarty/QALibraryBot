a6c66394e37bbe5bdb9c9eb21d860e209f77e248
OlaMarty <ok9039039898@gmail.com>
2022-06-30 09:08:36 +0700

    add links NDM

--- ndmLinks.go
+++ ndmLinks.go
@@ -2,16 +2,20 @@ package main
 
 var ndmLinks = []buttonLink{
 	{
+		Name: "Шпаргалка тестировщика",
+		Link: "https://elar.urfu.ru/bitstream/10995/28187/1/m_th_e.s.ivanov_2014.pdf",
+	},
+	{
 		Name: "Onboarding QA",
 		Link: "https://docs.google.com/document/d/11R7JGTAjgGuChu3t2i1JlMJUFxTnFZ_rkhRy6Ubaj1c/edit#heading=h.fadtgwlejzu7",
 	},
 	{
-		Name: "Схема тестирования",
-		Link: "https://docs.google.com/document/d/1Hd0ySkjInmkx_63rIhwXdcKjMf71pPW6YAISZ7fJE0k/edit#heading=h.rvgamgo8dq3s",
-	},
-	{
 		Name: "Стратегия тестирования",
 		Link: "https://docs.google.com/document/d/1H9mHd4wwWnfJ0407DdzOxV9T6DjK4ozVpGb-iNWG2fo/edit#heading=h.ccojlowc1457",
+	},
+	{
+		Name: "MiroMindMap",
+		Link: "https://miro.com/app/board/uXjVOs5OeDs=/?share_link_id=432357833385",
 	},
 	{
 		Name: "Грумминг автотестов",
@@ -22,7 +26,7 @@ var ndmLinks = []buttonLink{
 		Link: "https://docs.google.com/document/d/1D57xk5hBGbrfDQqfUGQyT0Q-HHhUNsCuciKbW1EDjo8/edit#",
 	},
 	{
-		Name: "MiroMindMap",
-		Link: "https://miro.com/app/board/uXjVOs5OeDs=/?share_link_id=432357833385",
+		Name: "Схема тестирования",
+		Link: "https://docs.google.com/document/d/1Hd0ySkjInmkx_63rIhwXdcKjMf71pPW6YAISZ7fJE0k/edit#heading=h.rvgamgo8dq3s",
 	},
 }
